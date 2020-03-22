da009436e9f2b3f459157b447b7be18113a3270d
liudonghua123 <liudonghua123@users.noreply.github.com>
2020-03-22 13:06:33 +0000

    deploy: 2dd4acb562e4cb7f5ff8fe9d0a9bb1ab83d05824

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,12 +4,12 @@ const RESOURCES = {
   "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/main.dart.js": "41066c43b06e985269386e7a2356a89c",
+"/main.dart.js": "9b10bf0b9fa8637f0e4959fdd3f84e61",
 "/manifest.json": "73974f27b05f20b4cb13d13177ec9f50",
 "/index.html": "4934c2de71727ef1c40d7f0b23371e13",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
+"/assets/LICENSE": "964211db6a8b173b1744e68da77ce459",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9"
 };
@@ -33,9 +33,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });

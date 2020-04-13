fd8da8a0a18ffbaebab4cce8a6129a9836593e66
liudonghua123 <liudonghua123@gmail.com>
2020-04-13 19:19:42 +0800

    update vendor specific files

--- windows/runner/window_configuration.cpp
+++ windows/runner/window_configuration.cpp
@@ -1,6 +1,6 @@
 #include "window_configuration.h"
 
-const wchar_t *kFlutterWindowTitle = L"flutter_app";
+const wchar_t* kFlutterWindowTitle = L"flutter_app";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
 const unsigned int kFlutterWindowWidth = 800;

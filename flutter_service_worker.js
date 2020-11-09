9583bb0fb09ed0df4d367462cf1506f8ba2fea8e
liudonghua123 <liudonghua123@users.noreply.github.com>
2020-11-09 02:06:26 +0000

    deploy: 660955b653bc45f8e6ac91fe71790c681e7cb7be

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,10 +10,10 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "manifest.json": "7b48859b17d6f9351202e2757b340fed",
-"main.dart.js": "76d2632a4e0c7dded9807ee83518e36c",
+"main.dart.js": "4cef6703d0517207352a585020233f75",
 "version.json": "21b60d415374031526ebe304809a0f7a",
-"index.html": "8bbf1ced58c3c60778319a1afad57597",
-"/": "8bbf1ced58c3c60778319a1afad57597",
+"index.html": "7ece8bdaa3efb15a281551142ebce503",
+"/": "7ece8bdaa3efb15a281551142ebce503",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
